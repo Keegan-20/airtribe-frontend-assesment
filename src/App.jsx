e901426ef60ec6db9a33217dd98391aa5d1bc2a3
Keegan <keegancolaco20@gmail.com>
2024-08-20 22:31:46 +0530

    ft: Storing tasks in Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import TaskModal from "./components/TaskModal/TaskModal";
 import { useState,useEffect} from "react";
 
 function App() {
+
   return (
     <div className="app">
       <TaskModal setAllTasks={setAllTasks} />
